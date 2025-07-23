bf8d81ba5b7c5e0c98f2d9991ee95ea22d4e2fe7
Florian de Haan <floriandehaan@example.com>
2025-07-23 10:24:04 +0200

    Fix sanity config import path and add React import for NextStudio page

--- nextjs project/app/studio/[[...index]]/page.tsx
+++ nextjs project/app/studio/[[...index]]/page.tsx
@@ -1,5 +1,6 @@
+import React from 'react'
 import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config.js'
+import config from '../../../../sanity.config.ts'
 
 export default function StudioPage() {
   return <NextStudio config={config} />
