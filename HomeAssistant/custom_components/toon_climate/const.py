b8d21bca1b3192ab5104d72436f15471203311cd
Florian de Haan <floriandehaan@example.com>
2025-07-23 13:06:27 +0200

    Remove HomeAssistant folder from repository

--- HomeAssistant/custom_components/toon_climate/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the Toon Thermostat."""
-
-DOMAIN = "climate"
-
-ACTIVE_STATE_AWAY = 3
-ACTIVE_STATE_COMFORT = 0
-ACTIVE_STATE_HOLIDAY = 4
-ACTIVE_STATE_HOME = 1
-ACTIVE_STATE_SLEEP = 2
