e36957510c79ef26a6812b8fae2739a39a409178
WORKFL0 <florian@workflo.nl>
2025-08-05 17:14:45 +0200

    Fix build error - remove unnecessary ServicesSectionServer
    ServicesSection component has hardcoded services and doesn't accept props

--- components/sections/ServicesSectionServer.tsx
+++
@@ -1,7 +0,0 @@
-import { notionFetch } from '@/lib/notion.fetch'
-import ServicesSection from './ServicesSection'
-
-export default async function ServicesSectionServer() {
-  const services = await notionFetch('services')
-  return <ServicesSection services={services} />
-}
